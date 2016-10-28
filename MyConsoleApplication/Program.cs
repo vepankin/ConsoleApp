8ea4b1846f27b41c17db511d5c4efd5b154dabfc
vepankin <vepankin@gmail.com>
2016-10-28 13:14:09 +0300

    hello world

--- MyConsoleApplication/Program.cs
+++ MyConsoleApplication/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyConsoleApplication
+﻿namespace MyConsoleApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Hello World!");
         }
     }
 }
